c17656f0e931138e7195dbc30a5a6d1646058895
Ismail BH <benhamouda.ismail@gmail.com>
2018-01-20 14:23:34 +0100

    add ws get by uuid and create book

--- api/book/book.controller.js
+++ api/book/book.controller.js
@@ -20,6 +20,17 @@ function handleEntityNotFound(res, statusCode) {
   }
 }
 
+function handleEntityNotFoundObject(res, uuid){
+  return books => {
+    const data = books.find(book => book.uuid === uuid);
+    if(!data){
+      res.status(404).end({});
+      return null;
+    }
+    return data;
+  }
+}
+
 function handleError(res, statusCode) {
   statusCode = statusCode || 500;
   return err => {
@@ -39,5 +50,21 @@ exports.show = (req, res) => {
   Book.findByIdAsync(req.params.id)
     .then(handleEntityNotFound(res))
     .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+// Gets a single User from DB with uuid
+exports.showUuid = (req, res) =>{
+  Book.findAsync()
+  .then(handleEntityNotFoundObject(res, req.params.uuid))
+  .then(respondWithResult(res))
+  .catch(handleError(res))
+}
+
+// Create a new Book in the DB
+exports.create = (req, res) => {
+  req.body.book.uuid = uuid();
+  Book.createAsync(req.body.book)
+    .then(respondWithResult(res, 201))
     .catch(handleError(res));
 }
--- api/book/index.js
+++ api/book/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-var { index, show } = require('./book.controller');
-
+var { index, show, showUuid, create } = require('./book.controller');
 
 router.get('/', index);
 router.get('/getId/:id', show);
+router.get('/getUuid/:uuid', showUuid);
+router.post('/', create);
 
 module.exports = router;
