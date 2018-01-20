22614eab672263c45dd7eb6dc046503b80fa18a4
Ismail BH <benhamouda.ismail@gmail.com>
2018-01-20 12:04:57 +0100

    change project structur

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
+
+app.use('/api/users', require('../api/user'));
+app.use('api/books', require('../api/book'));
+
+module.exports = app;
 
 
