c8074fb449686f338cdf261bbbdd9111f048b17b
Ismail BH <benhamouda.ismail@gmail.com>
2018-01-20 14:08:32 +0100

    add data to db

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./routes'));
 
 // seed DB
-// require('./config/seed');
+require('./config/seed');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
